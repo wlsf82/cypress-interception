3174e07732508485c2cb21526e4b5a026d4c2b31
Walmyr <wlsf82@gmail.com>
2026-07-09 19:44:39 +0200

    Change the default reporter

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   allowCypressEnv: false,
+  reporter: 'nyan',
 
   e2e: {
     baseUrl: "https://wlsf82-hacker-stories.web.app",
